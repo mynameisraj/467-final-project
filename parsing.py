d0c5bdd48b820f7195f6dd8e760294af2750a2ff
Mayank Jain <mjmayank@gmail.com>
2014-04-29 23:03:11 -0500

    kavya's new parser that arranges by date

--- parsing.py
+++ parsing.py
@@ -12,9 +12,9 @@ dates = []
 dateArray = []
 
 dateObject = ['','']
+myDict = {}
 
-
-file2 = open('sorted.json', 'w')
+file2 = open('myJson_test.json', 'w')
 for line in infile:
 	line.strip()
 	if not (line.find("|2014-")==-1):
@@ -22,28 +22,36 @@ for line in infile:
 		splitIndex = line.find("|2014") #find out where to split the damn string
 		splitIndex2 = line.find("|")
 		
-		dateArrayIndex_date = line[splitIndex+1:splitIndex+20] #copy date into a var
+		dateArrayIndex_dateTime = line[splitIndex+1:splitIndex+20] #copy date into a var
 		dateArrayIndex_url = line[0:splitIndex2]	#copy url into a var
-		
+		dateArrayIndex_justDate = line[splitIndex+1:splitIndex+11]
+		dateArrayIndex_justTime = line[splitIndex+12:splitIndex+20]
+
+		myTuple = (dateArrayIndex_justTime, dateArrayIndex_url)
+		#print dateArrayIndex_justDate
+
+		if not (dateArrayIndex_justDate in myDict):
+			myDict[dateArrayIndex_justDate] = [] 
+
+		myDict[dateArrayIndex_justDate].append(myTuple)
+
 		#append both url and date vars into one 'dates' obj  
-		dates2 = dateArrayIndex_date, dateArrayIndex_url
-		dateArray.append(dates2)
+		#dates2 = dateArrayIndex_dateTime, dateArrayIndex_url
+		#dateArray.append(dates2)
 		#print '\n'
+print myDict
 
-newlist = sorted(dateArray, key=lambda k: k[0]) 
-
-for dates2 in dateArray:
-	print dates2
+#for x in myDict:
+	#print x + str(myDict[x])
 
 		#'dateArray' is an array of 'dates' objects
 	
-	#print dateArray
+	#print dateArrays
 	#for data in dateArray:
 	#	print data
-for dates2 in newlist:
 
-	x  = json.dumps(dates2)
-	file2.write(x)
+d  = json.dumps(myDict, indent=2)
+file2.write(d + "\n")
 
 
 '''for data in dateArray:
