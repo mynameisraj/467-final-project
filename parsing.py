ed92838bd06ff4ed96a5400604b7ec84ad906fe4
dlquach <derekq.alt@gmail.com>
2014-04-30 22:40:31 -0500

    Parser now outputs JSON object with unix timestamps as keys and urls as values

--- parsing.py
+++ parsing.py
@@ -1,6 +1,9 @@
 import json
 import urllib2
 import itertools
+from datetime import datetime
+import dateutil.parser
+import time
 
 infile = open('urls.txt', 'r')
 print("the file has succesfully opened")
@@ -16,48 +19,32 @@ myDict = {}
 
 file2 = open('myJson_test.json', 'w')
 for line in infile:
-	line.strip()
-	if not (line.find("|2014-")==-1):
-		
-		splitIndex = line.find("|2014-") #find out where to split the damn string
-		splitIndex2 = line.find("|")
-		
-		dateArrayIndex_dateTime = line[splitIndex+1:splitIndex+20] #copy date into a var
-		dateArrayIndex_url = line[0:splitIndex2]	#copy url into a var
-		dateArrayIndex_justDate = line[splitIndex+1:splitIndex+11]
-		dateArrayIndex_justTime = line[splitIndex+12:splitIndex+20]
+    line.strip()
+    if not (line.find("|2014-")==-1):
+        splitIndex = line.find("|2014-") #find out where to split the damn string
+        splitIndex2 = line.find("|")
+        
+        dateArrayIndex_dateTime = line[splitIndex+1:splitIndex+20] #copy date into a var
+        dateArrayIndex_url = line[0:splitIndex2]    #copy url into a var
+        dateArrayIndex_justDate = line[splitIndex+1:splitIndex+11]
+        dateArrayIndex_justTime = line[splitIndex+12:splitIndex+20]
 
-		myTuple = (dateArrayIndex_justTime, dateArrayIndex_url)
-		#print dateArrayIndex_justDate
+        myTuple = (dateArrayIndex_justTime, dateArrayIndex_url)
+        text_date = dateArrayIndex_justDate + " " + dateArrayIndex_justTime
+        test = dateutil.parser.parse(text_date)
+        unix_time = time.mktime(test.timetuple())+1e-6*test.microsecond
+        
+        myDict[unix_time] = dateArrayIndex_url
 
-		if not (dateArrayIndex_justDate in myDict):
-			myDict[dateArrayIndex_justDate] = {} 
-
-		myDict[dateArrayIndex_justDate][dateArrayIndex_justTime] = dateArrayIndex_url
-
-		#append both url and date vars into one 'dates' obj  
-		#dates2 = dateArrayIndex_dateTime, dateArrayIndex_url
-		#dateArray.append(dates2)
-		#print '\n'
-
-#for x in myDict:
-	#print x + str(myDict[x])
-
-		#'dateArray' is an array of 'dates' objects
-	
-	#print dateArrays
-	#for data in dateArray:
-	#	print data
 
 d  = json.dumps(myDict, indent=2)
-print d
 file2.write(d)
 file2.close()
-
+print d
 
 '''for data in dateArray:
-	print dateArray
-	print '\n'
-		#dateArray is where all your dates and URLs are stored in the format ['date','url']
+    print dateArray
+    print '\n'
+        #dateArray is where all your dates and URLs are stored in the format ['date','url']
 '''
 
